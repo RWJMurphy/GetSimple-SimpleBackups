fc6ba2b0f52bb9c05623d5eacbb6b17e6454c21f
Reed Murphy <reed@reedmurphy.net>
2012-03-20 11:29:01 +1100

    Minor stylistic tweaks to run_backip

--- simplebackups/pages/run_backup.php
+++ simplebackups/pages/run_backup.php
@@ -29,28 +29,28 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 $last = $data['last_run'][0];
 ?>
 <h2><?php i18n(SB_SHORTNAME.'/RUN_BACKUP_NOW'); ?></h2>
-<form action="<?php echo sb_link("run_backup"); ?>" method="POST">
-<?php i18n(SB_SHORTNAME.'/BACKUP'); ?>
+<form id="run" action="<?php echo sb_link("run_backup"); ?>" method="POST">
+<?php i18n(SB_SHORTNAME.'/BACKUP'); ?> 
 <select name="source">
     <option value="" disabled="disabled"><?php i18n(SB_SHORTNAME.'/SOURCE'); ?></option>
 <?php foreach($data['sources'] as $key => $source): ?>
     <option <?php if ($last['source'] == $key) { echo 'selected="selected" '; } ?> value="<?php echo $key; ?>" ><?php echo $source['name']; ?></option>
 <?php endforeach; ?>
 </select>
-<?php i18n(SB_SHORTNAME.'/TO'); ?>
+<?php i18n(SB_SHORTNAME.'/TO'); ?> 
 <select name="destination">
     <option value="" disabled="disabled"><?php i18n(SB_SHORTNAME.'/DESTINATION'); ?></option>
 <?php foreach($data['destinations'] as $key => $destination): ?>
     <option <?php if ($last['destination'] == $key) { echo 'selected="selected" '; } ?> value="<?php echo $key; ?>" ><?php echo $destination['name']; ?></option>
 <?php endforeach; ?>
 </select>
-<?php i18n(SB_SHORTNAME.'/AS'); ?>
+<?php i18n(SB_SHORTNAME.'/AS'); ?> 
 <select name="format">
     <option value="" disabled="disabled"><?php i18n(SB_SHORTNAME.'/ARCHIVE_FORMAT'); ?></option>
 <?php foreach($data['archive_formats'] as $format): ?>
     <option <?php if ($last['archive_format'] == $format) { echo 'selected="selected" '; } ?> value="<?php echo $format; ?>" ><?php echo $format; ?></option>
 <?php endforeach; ?>
-</select>,<?php i18n(SB_SHORTNAME.'/KEEPING'); ?> <input name="limit" size="2" <?php if ($last['limit']) { echo 'value="' . $last['limit'] . '" '; } ?> /> <?php i18n(SB_SHORTNAME.'/BACKUPS'); ?>.
+</select>, <?php i18n(SB_SHORTNAME.'/KEEPING'); ?> <input name="limit" size="2" <?php if ($last['limit']) { echo 'value="' . $last['limit'] . '" '; } ?> /> <?php i18n(SB_SHORTNAME.'/BACKUPS'); ?>.
 
 <input type="submit" value="<?php i18n(SB_SHORTNAME.'/GO!'); ?>" />
 </form>
