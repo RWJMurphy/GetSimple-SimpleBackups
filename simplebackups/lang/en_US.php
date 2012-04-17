87b59a9af72329a70d85f085094671b3f736026e
Reed Murphy <reed@reedmurphy.net>
2012-04-18 09:56:41 +1000

    Minor grammar

--- simplebackups/lang/en_US.php
+++ simplebackups/lang/en_US.php
@@ -135,7 +135,7 @@ $i18n = array(
     'DELETE_LOG' => "Delete log",
     'RUN_BACKUP_NOW' => "Run Backup Now",
 
-    'FREE_EMAIL_WARNING' => "Please note: Free email accounts like Gmail, will probably reject your backups.",
+    'FREE_EMAIL_WARNING' => "Please note: Free email accounts, like Gmail, will probably reject your backups.",
 
     'BACKUP' => "Backup",
     "BACKUPS" => "backups",
